33f32bf0c8700acfcfe280b1027493365e838149
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-05-23 09:45:42 +0300

    Merged both servers into one file

--- routes/signin.js
+++ routes/signin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passwordHash = require('password-hash');
 var mongo = require('mongodb').MongoClient;
+var Cookie = require('cookies');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,7 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 	res.clearCookie('logged');
 	res.clearCookie('username');
+	var cookies = new Cookie(req, res);
 	
 	//Connectt to Mongodb and do all of that
 	mongo.connect("mongodb://127.0.0.1/chat", function(err, db) {
@@ -22,10 +24,12 @@ router.post('/', function(req, res, next) {
 		users.findOne({username: req.body.username}, {"_id": false, "password": true, uniq: true}, function(err, item) {
 			if(item){
 				if(passwordHash.verify(req.body.password, item.password)) {
-					res.render('index', { uniq: item.uniq });
+					//SET COOKIES at 6 months, delete on index if not good
+					cookies.set('username', req.body.username, {maxAge: 10000, path: '/'});
 					if(req.body.remember == "") {
 						
 					}
+					res.render('index', { uniq: item.uniq });
 				}
 			}
 		});
